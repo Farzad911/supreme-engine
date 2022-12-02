1e4d7552ea7656d2079d9c4ce1b8a8a5f0a73730
Farzad911 <119702680+Farzad911@users.noreply.github.com>
2022-12-02 15:48:47 -0500

    Update fundementals.h

--- fundementals.h
+++ fundementals.h
@@ -1,14 +1,12 @@
-// FUNDAMENTALS MODULE HEADER 
+// that is the converting module header
 #ifndef _FUNDAMENTALS_H_
 #define _FUNDAMENTALS_H_
-
+// this is the libraries
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h>
-
 //V1  Takes non-empty string and a number as input and gives the character at that number(index) as output. 
-//V2  Takes non-empty string and gives the number of characters as output
-//V3  Takes non-empty string and copies it to a new string 
+// This is the function that we use when we aply a convertion
 void fundamentals(void);
 
 #endif
