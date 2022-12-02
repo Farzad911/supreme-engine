5abffa752618b4bff8f126a6ab1b56f5c71fb1d3
Farzad911 <119702680+Farzad911@users.noreply.github.com>
2022-12-02 16:00:52 -0500

    Update tokenizing.h

--- tokenizing.h
+++ tokenizing.h
@@ -1,10 +1,11 @@
-// Tokenization header file
+// that is the converting module header
 #ifndef _TOKENIZING_H_
 #define _TOKENIZING_H_
-
+// this is the libraries
 #include <stdio.h>
 #include <string.h>
-
+// Prototype  Function 
+// This is the function that we use when we aply a convertion
 void tokenizing(void); // Purpose: This function takes a user inputted string and tokenizes it
 
 #endif
