3cfb45d64cfe57a866dfd01e225eb42ca051c833
Farzad911 <119702680+Farzad911@users.noreply.github.com>
2022-12-02 15:56:09 -0500

    Update manipulating.h

--- manipulating.h
+++ manipulating.h
@@ -1,11 +1,11 @@
-//MANIPULATING MODULE HEADER
+// that is the converting module header
 #ifndef _MANIPULATING_H_
 #define _MANIPULATING_H_
-
+// this is the libraries
 #include <stdio.h>		// For input/output and printing
 #include <string.h>		// For string manipulation functions
-
+// This is the function that we use when we aply a convertion
 void manipulating(void);
-/* Purpose: This function concatenates 2 input strings */
+/* Purpose: This function contains 2 input strings */
 
 #endif
