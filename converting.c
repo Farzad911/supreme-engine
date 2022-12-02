90c79b418a88908bff9f829f5085d05396a0a485
Farzad911 <119702680+Farzad911@users.noreply.github.com>
2022-12-02 15:13:27 -0500

    Create converting.c

---
+++ converting.c
@@ -0,0 +1,13 @@
+// that is the converting module header
+#ifndef _CONVERTING_H_
+#define _CONVERTING_H_
+
+// this is the libraries
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+// Prototype  Function 
+// This is the function that we use when we aply a convertion
+void converting(void);
+
+#endif
