69160507077127df25dbda3a0ea401b39c0f41ca
Farzad911 <119702680+Farzad911@users.noreply.github.com>
2022-12-02 15:36:47 -0500

    Create fundementals.h

---
+++ fundementals.h
@@ -0,0 +1,14 @@
+// FUNDAMENTALS MODULE HEADER 
+#ifndef _FUNDAMENTALS_H_
+#define _FUNDAMENTALS_H_
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h>
+
+//V1  Takes non-empty string and a number as input and gives the character at that number(index) as output. 
+//V2  Takes non-empty string and gives the number of characters as output
+//V3  Takes non-empty string and copies it to a new string 
+void fundamentals(void);
+
+#endif
