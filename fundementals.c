f03dee70d0e3e107526e5429c2e588b561f8cf24
Farzad911 <119702680+Farzad911@users.noreply.github.com>
2022-12-06 12:49:56 -0500

    Update fundementals.c

--- fundementals.c
+++ fundementals.c
@@ -13,7 +13,7 @@ void fundamentals(void) {
     size_t position; 
      // It start a do-while loop
     do {
-        printf("Type not empty string (q - to quit): \n");  // printing instructionstatement
+        printf("Type not empty string (q - to quit): \n");  // printing instruction statement
         fgets (buffer1, BUFFER_SIZE, stdin); // Prompt user for a integer numeric string
         buffer1[strlen (buffer1) - 1] = '\0'; // Replaces user entered newline with null terminator
 
@@ -34,3 +34,23 @@ void fundamentals(void) {
     } while (strcmp (buffer1, "q") != 0); // breaks the loop if user inputs 'q' as the only character
     // The do-while loop end
     printf("*** End of Indexing Strings Demo ***\n\n"); // Output an ending statement to mark end of the module
+}
+/* Version 2 */
+
+ printf("*** Start of Measuring Strings Demo ***\n"); //printing statement for beginning the module.
+    char buffer2[BUFFER_SIZE]; // declaring array of char type 
+// Start a do-while loop
+    do { 
+        printf("Type a string (q - to quit):\n");  // printing instruction statement 
+        fgets(buffer2, BUFFER_SIZE, stdin); //Gets the user entered string
+        buffer2[strlen(buffer2) - 1] = '\0'; // Replaces user entered newline with null terminator 
+
+        // checks if entered string is not equals to 'q'.
+        if (strcmp(buffer2, "q") != 0) {
+            printf("The length of \'%s\' is %d characters\n", 
+            buffer2, (int)strlen(buffer2)); 
+        }
+    } while (strcmp(buffer2, "q") != 0); // breaks the loop if user inputs 'q' as the only character
+    // The do-while loop end
+    printf("*** End of Measuring Strings Demo ***\n\n");// Output an ending statement to mark end of the module
+}
